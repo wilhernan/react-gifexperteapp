df3e0b173d468f7f81fd9295fd5fc6ed1ea4875f
Wilfredo Hernández <wilhernan777@gmail.com>
2022-01-06 16:08:41 -0400

    docs add

---
+++ docs/precache-manifest.234a37434d8ba7c27998b9e772e93e04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8769231c21b7f2779b259f756e062f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dbdb323df26bb952bbc",
+    "url": "/static/css/main.65dd21a6.chunk.css"
+  },
+  {
+    "revision": "7fbf4d4a85a3639b2586",
+    "url": "/static/js/2.441c8ca0.chunk.js"
+  },
+  {
+    "revision": "fa292ceceacf968de0ff262d170d4f36",
+    "url": "/static/js/2.441c8ca0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dbdb323df26bb952bbc",
+    "url": "/static/js/main.294614c5.chunk.js"
+  },
+  {
+    "revision": "5fc9eb9b68aaf01b9f5c",
+    "url": "/static/js/runtime-main.1a6603c2.js"
+  }
+]);
